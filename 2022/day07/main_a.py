c60fe7ec70339da48e3231fae5681a78fcae04ab
tasanast <29357446+tasanast@users.noreply.github.com>
2023-10-11 12:40:55 +0200

    added function to calculate sum of size of folders with a maximum value

--- 2022/day07/main_a.py
+++ 2022/day07/main_a.py
@@ -1,9 +1,19 @@
-# adventofcode.com 2022 day99
+# adventofcode.com 2022 day07 part 1
 
 import sys
-from node_template import get_node 
+from node_template import Node
+
+def get_sum(max_size, parent_node):
+    total_sum = 0
+    if parent_node.is_folder():
+        if parent_node.get_size() <= max_size:
+            total_sum += parent_node.get_size()
+        for node in parent_node.children:
+            total_sum += get_sum(max_size, node)
+    return total_sum
 
 data = []
+root_folder = Node("/")
 
 def main(input_filename):
     with open(input_filename) as file:
@@ -11,15 +21,31 @@ def main(input_filename):
         while line:
             data.append(line.strip())
             line = file.readline()
+    
+    # Build folder tree
 
+    current_folder = root_folder
     for line in data:
         words = line.split(" ")
-
+        
+        # Handle commands 
         if words[0] == "$":
             if words [1] == "cd":
-                current_directory = words[2]
-                parrent_directory = 
+                if words[2] == "/":
+                    current_folder = root_folder
+                else:
+                    current_folder = current_folder.cd(words[2])
 
+        # add children to current folder
+        elif words[0] == "dir":
+            current_folder.add_child(Node(words[1]))
+        else: 
+        # add file to the children list
+            current_folder.add_child(Node(words[1], int(words[0])))
+
+    # get sum
+    print(get_sum(100000,root_folder))
+    
 
 
 if __name__ == "__main__":
