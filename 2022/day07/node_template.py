c6743a9bf2e27a337314d7cabf2e3b8c3b8000c1
Tas Anast <29357446+tasanast@users.noreply.github.com>
2023-10-11 10:44:31 +0200

    latest node template

--- 2022/day07/node_template.py
+++ 2022/day07/node_template.py
@@ -1,9 +1,17 @@
-def get_node(name, parent_folder, node_type, size=0):
-    node = {
-                'name': name,
-                'parent_folder': parent_folder,
-                'node_type': node_type,
-                'size': size
-            }
+class Node:
+    parent_folder = None
+    children = []
+    size = 0
 
-    return node
+    def __init__(self, size):
+        self.size = size
+
+    def get_size(self):
+        total_size = self.size
+        for node in children:
+            size += node.get_size()
+
+        return total_size
+
+    def add_child(child_node):
+        childrent.append(child_node)
