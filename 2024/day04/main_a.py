cacba2b05db5a38a04f6f2de5507185a70350a82
tasanast <29357446+tasanast@users.noreply.github.com>
2024-12-20 10:21:27 +0000

    added 2024 day04. Still incomplete

---
+++ 2024/day04/main_a.py
@@ -0,0 +1,10 @@
+# advent of code 2024 day04 part 1
+
+file_name = 'input.txt'
+
+data = []
+with open(file_name) as f:
+    data = [line.strip() for line in f]
+
+print(data) # check that data was read correctly
+
